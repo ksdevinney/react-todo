c0d78421f604118d76e7ef4a65af9d817c089a56
ksdevinney <katie@devinney.org>
2021-06-24 14:59:32 -0500

    conditional formatting toggle

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,9 @@
 import { FaTimes } from 'react-icons/fa'
 import React from 'react'
 
-function Task({ task, onDelete }) {
+function Task({ task, onDelete, onToggle }) {
     return (
-        <div className='task'>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)} >
             <h3>{task.text} 
             <FaTimes style={{ color: 'red' }} 
             onClick={() => onDelete(task.id)} /> 
